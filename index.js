1e8abc77de19bfc0e6c1f13f4cd4641af3e06177
Carl Colglazier <carltcolglazier@gmail.com>
2015-01-13 08:24:56 -0500

    Added more of the framework on THUMS itself
    + Declare application type in config.json
    + Removed .gitattributes
    + Run sockets through THUMS
    + Render though THUMS

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ var app = require('express')(), // Express framework for web applications http:/
     hbs = require('hbs'), // Express wrapper for Handlebars https://github.com/donpark/hbs
     fs = require('fs'),
     path = require('path'),
-    server = require('http').Server(app); // Start server with Express and http
+    server = require('http').Server(app), // Start server with Express and http
+    io = require('socket.io')(server); // Socket.io real-time engine http://socket.io/
 
 app.set('view engine', 'hbs'); // Connect handlebars to Express
 hbs.registerPartials(__dirname+'/views/partials'); // Designate partials folder for handlebars
@@ -13,12 +14,15 @@ fs.readFile( path.join(__dirname, 'config.json'), {encoding: 'utf-8'}, function(
         for (var x = 0; x < data["apps"].length; x++) {
             console.log(data["apps"][x]["name"]);
             var new_mid = require(data["apps"][x]["name"]);
-            if (new_mid.connect) {
-                new_mid.connect(server);
+            if (data["apps"][x]["type"].indexOf("sockets") >= 0) {
+                io.on('connection',new_mid);
             }
-            app.use(data["apps"][x]["path"],new_mid);
+            //app.use(data["apps"][x]["path"],new_mid);
         }
     }
+    app.get('/', function (req, res) { // Index - used for client input
+        res.render('index');
+    });
 })
 
 
