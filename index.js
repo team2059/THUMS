10bffb2d736612b7418da0f571feb00e693b04a3
Carl Colglazier <carltcolglazier@gmail.com>
2015-01-12 23:05:23 -0500

    Modular app system
    + Apps can now be loaded through config.json (they must still be
    installed through package.json for the time being).
    + git now ignores SQLite files.
    + Server now runs directly through THUMS
    + Scouting module no longer hard coded.

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
-var Scouting = require('2015-Scouting');
-var app = require('express')(); // Express framework for web applications http://expressjs.com/
-var hbs = require('hbs'); // Express wrapper for Handlebars https://github.com/donpark/hbs
+var app = require('express')(), // Express framework for web applications http://expressjs.com/
+    hbs = require('hbs'), // Express wrapper for Handlebars https://github.com/donpark/hbs
+    fs = require('fs'),
+    path = require('path'),
+    server = require('http').Server(app); // Start server with Express and http
 
 app.set('view engine', 'hbs'); // Connect handlebars to Express
 hbs.registerPartials(__dirname+'/views/partials'); // Designate partials folder for handlebars
 
-app.use(Scouting);
+fs.readFile( path.join(__dirname, 'config.json'), {encoding: 'utf-8'}, function(err,data){
+    if (!err){
+        data = JSON.parse(data);
+        for (var x = 0; x < data["apps"].length; x++) {
+            console.log(data["apps"][x]["name"]);
+            var new_mid = require(data["apps"][x]["name"]);
+            if (new_mid.connect) {
+                new_mid.connect(server);
+            }
+            app.use(data["apps"][x]["path"],new_mid);
+        }
+    }
+})
+
+
+
+server.listen(80);
