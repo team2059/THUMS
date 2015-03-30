678a72ac21ca4636d40579fd0fc2f6a172132876
Carl Colglazier <carltcolglazier@gmail.com>
2015-03-29 20:45:33 -0400

    Add user authentication.

--- content/apps/THUMS-Admin/index.js
+++ content/apps/THUMS-Admin/index.js
@@ -1,27 +1,95 @@
 module.exports = (function () {
     'use strict';
-    var db = null,
-        routes = [{
-            method: 'GET',
-            path: '/admin/{page?}',
-            id: 'THUMS-Admin',
-            handler: function (request, reply) {
-                if (db) {
-                    db.getPostsByCategory('blog')
-                        .then(function (posts) {
-                            reply(posts.map(function (post) {
-                                return {id: post.id,
-                                    type: 'postlink',
-                                    title: post.title,
-                                    slug: post.slug
-                                };
-                            }));
-                        })
-                } else {
-                    reply(null);
+    var login_section = [{
+            id: 1,
+            title: 'Login',
+            type: 'login',
+            content: {
+                register: false,
+                login: true
+            }
+        }],
+        register_section = [{
+            id: 1,
+            title: 'Register',
+            type: 'login',
+            content: {
+                register: true,
+                login: false
+            }
+        }],
+        db = null,
+        routes = [
+            {
+                path: '/admin/{page?}',
+                id: 'THUMS-Admin',
+                handler: function (request, reply) {
+                    if (!request.auth.isAuthenticated) {
+                        reply(login_section);
+                    }
+                    if (db) {
+                        db.getPostsByCategory('blog')
+                            .then(function (posts) {
+                                reply(posts.map(function (post) {
+                                    return {id: post.id,
+                                        type: 'postlink',
+                                        title: post.title,
+                                        slug: post.slug
+                                    };
+                                }));
+                            })
+                    } else {
+                        reply(null);
+                    }
+                }
+            }, {
+                path: '/admin/login',
+                id: 'THUMS-Admin-login',
+                handler: function (request, reply) {
+                    if (request.auth.isAuthenticated) {
+                        return reply([]);
+                    }
+                    if (request.method === 'get') {
+                        db.count('users', 'id')
+                            .then(function (num) {
+                                if (num) {
+                                    return reply(login_section);
+                                } else {
+                                    console.log('no user!');
+                                    return reply(register_section);
+                                }
+                            });
+                    }
+                    if (request.method === 'post') {
+                        if (request.payload.email) {
+
+                            // New user registration.
+                            db.count('users', 'id') // Make sure there are no users.
+                                .then(function (num) {
+                                    if (!num) {
+                                        db.addUser(request.payload.email,
+                                            request.payload.username,
+                                            request.payload.password)
+                                            .then(function () {
+                                                reply([]);
+                                            })
+                                    } else {
+                                        return reply(login_section);
+                                    }
+                                });
+                        } else {
+                            db.authenticateUser(request.payload.username, request.payload.password)
+                                .then(function (match_object) {
+                                    request.auth.session.set(match_object);
+                                    return reply([]);
+                                }).catch(function () {
+                                    return reply([]);
+                                });
+                        }
+                    }
                 }
             }
-        }];
+        ];
 
     function database(data) {
         'use strict';
